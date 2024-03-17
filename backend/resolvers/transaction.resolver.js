1e78233bd524966a8a0f740434f28cba6ff7bc36
sahil <sahil27raj@gmail.com>
2024-03-17 19:05:42 +0530

    user and transaction resolvers added

--- backend/resolvers/transaction.resolver.js
+++ backend/resolvers/transaction.resolver.js
@@ -2,10 +2,69 @@ import {users} from '../dummyData/data.js'
 
 const transactionResolver = {
     Query:{
-        users:()=>{
-            return users
+        transactions:async(_,__, context)=>{
+            try{
+                if(!context.getUser())
+                throw new Error("Unauthorized");
+                const userId = await context.getUser()._id;
+
+                const transaction = await Transaction.find({userId:userId});
+                return transaction;
+
+            }catch(error){
+                console.log("error getting transactions:",error);
+                throw new Error("Error getting transaction"); 
+            }
+        },
+        transaction:async(_,{ transactionId},)=>{
+        try{
+            const transaction = await Transaction.findById(transactionId);
+            return transaction;
+        }catch(error){
+            console.log("error getting transaction:",error);
+                throw new Error("Error getting transaction"); 
+
+        }
         }
     },
-    Mutation:{}
+    Mutation:{
+        createTransaction: async(_,{input},context) =>{
+            try{
+                const newTransaction = new Transaction({
+                    ...input,
+                    userId:context.getUser()._id
+                })
+
+                await newTransaction.save();
+                return newTransaction;
+            }catch(error){
+                console.log("error Creating  transaction:",error);
+                throw new Error("Error Creating transaction"); 
+            }
+        },
+        updateTransaction: async(_,{input})=>{
+            try{
+                const updatedTransaction = await Transaction.findByIdAndUpdate(input.transactionId,input,{new:true});
+                return updatedTransaction;
+
+            }catch(error){
+                console.log("error Updating  transaction:",error);
+                throw new Error("Error Updating transaction"); 
+            }
+
+        },
+        deleteTransaction: async(_,{transactionId})=>{
+            try{
+                const deleteTransaction = await Transaction.findByIdAndDelete(transactionId);
+                return deleteTransaction;
+
+            }catch(error){
+                console.log("error Deleting  transaction:",error);
+                throw new Error("Error Deleting transaction"); 
+            }
+        },
+
+
+    }
 }
 export default transactionResolver
--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -1,15 +1,101 @@
 import {users} from '../dummyData/data.js'
+import User from '../models/user.models.js';
+import bcrypt from 'bcryptjs';
 
 const userResolver = {
+     Mutation:{
+signUp:async(_,{input},context)=>{
+    try{
+        const {username,name,password,gender} = input;
+        if(!username || !name || !password  || !gender){
+            throw new Error("All fields are Required");
+        } 
+        const  existingUser = await User.findOne({username});
+  
+        if(existingUser){
+            throw new Error("User Alredy Exists");
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt);
+        // https://avatar-placeholder.iran.liara.run/ 
+
+        const boyProfilePic = `http://avatar-placeholder.iran.liara.run/public/boy/username=${username}`;
+        const girlProfilePic = `http://avatar-placeholder.iran.liara.run/public/girl/username=${username}`;
+
+        const newUser = new User({
+            username,
+            name,
+            password:hashedPassword,
+            gender,
+            profilePicture:gender === "male" ?boyProfilePic :girlProfilePic, 
+        })
+
+        await newUser.save();
+        await context.login(username)
+        return newUser;
+
+    }catch(error){
+        console.log("Error in Signup:",error);
+        throw new Error(error.message || "internal server Error");
+
+    }
+},
+
+ login: async(_,{input},context)=>{
+        try{
+            const {username,password}=input;
+            await context.authentication("graphql-local",{username,password})
+
+            await context.login(user);
+            return user
+
+        }catch(error){
+            console.log("Error in login ",error);
+            throw new Error(error.message || "Internal server error");
+        }
+
+    },
+    logout: async(_,__,context)=>{
+        try{
+            await context.logout();
+            req.session.destroy((err)=>{
+                if(err) throw err;
+            })
+            res.clearCookie("connect.sid");
+            return {message :"Logged Out Successfully"};
+
+        }catch(error){
+            console.log("Error in Logout ",error);
+            throw new Error(error.message || "Internal server error");
+        }
+
+    }
+     },
     Query:{
         // parents,args,context,info
-        users:(_,_,{req,res})=>{
-            return users
+        authUser:async(_,__,context)=>{
+            try{
+                const user = await context.getUser()
+                return user;
+
+            }catch(error){
+                console.log("Error in auth User ",error);
+                throw new Error(error.message || "Internal server error");
+            }
+
         },
-        user:(_,{userId})=>{
-            return users.find((user)=> user._id === userId);
+        user:async(_,{userId})=>{
+           try{
+            const user = await User.findById(userID);
+            return user;
+
+           }catch(error){
+            console.log("Error in User Query",error);
+            throw new Error(error.message || "Error in Getting User ");
+           }
         }
     },
-    Mutation:{}
+   
 }
 export default userResolver
